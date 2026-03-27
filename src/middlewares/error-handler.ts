838548283a074f04971fdd55e8274750cc4eac44
Arthur Lagenebre <lagenebre.arthur@gmail.com>
2026-03-27 13:24:35 +0100

    fix(errors): return proper http status codes

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -3,13 +3,14 @@ import { logger } from '../utils/logger.js';
 import type { NextFunction, Request, Response } from 'express';
 
 type AppError = {
+    status?: number;
     statusCode?: number;
     message?: string;
     code?: string;
 };
 
 export function errorHandler(err: AppError, _req: Request, res: Response, _next: NextFunction) {
-  const statusCode = err.statusCode ?? 500;
+  const statusCode = err.statusCode ?? err.status ?? 500;
 
   if (statusCode >= 500)
     logger.error('[http] Internal error', err);
